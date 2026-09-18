574fd571169e9d7ac97ac2bfaa624e3eca44bb7e
abdullahmassraf <132116762+abdullahmassraf@users.noreply.github.com>
2026-09-17 21:33:22 -0400

    Add files via upload

---
+++ Sheridan/Anthropology of Health ANTH17028GD/Unit01/Lec1_P2/scripts/config_xml.js
@@ -0,0 +1,62 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description tsc:version="2.0.1" dc:date="2015-07-07 11:26:23 AM" dc:source="Camtasia Studio,8.5.2,enu" dc:title="Lec1_P2" tscDM:firstFrame="Lec1_P2_First_Frame.png" tscDM:originId="DFE31CF5-9F8F-4607-86EF-627AFD27B5DB" tscDM:project="Untitled">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="255566"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="720" stDim:w="1152"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="ScreenText" xmpDM:frameRate="f1000" xmpDM:trackName="Screen Text">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Part 1 Define health, disease and illness" xmpDM:startTime="0" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health, Disease and Illness Health 081012-collaborative-public-health-research-©-kudryashka-fotolia1.jpg" xmpDM:startTime="6300" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="http://www.cam.ac.uk/sites/www.cam.ac.uk/files/news/research/news/081012-collaborative-public-health-research-%C2%A9-kudryashka-fotolia1.jpg" xmpDM:startTime="6300" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="" xmpDM:startTime="6300" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health, Disease and Illness http://www.differencebetween.info/sites/default/files/images5/signs-and-symptoms_0.jpg" xmpDM:startTime="39133" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="signs-and-symptoms_0.jpg" xmpDM:startTime="39133" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health Disease" xmpDM:startTime="39133" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health, Disease and Illness http://www.differencebetween.info/sites/default/files/images5/signs-and-symptoms_0.jpg" xmpDM:startTime="64933" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="signs-and-symptoms_0.jpg" xmpDM:startTime="64933" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="signs-and-symptoms_0.jpg" xmpDM:startTime="64933" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health Disease Illness" xmpDM:startTime="64933" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health and Illness Variation http://static1.squarespace.com/static/52d2fc5ce4b0ed895b7f3e99/t/539b1feae4b0d90b907f0318/1402675188791/" xmpDM:startTime="141767" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="" xmpDM:startTime="141767" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="How do we think about Health &amp; Illness? A ‘cold’ Discussion Discussion Go To … Discussions: Discussion 1 – How" xmpDM:startTime="172500" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="we thing about Health &amp; Illness" xmpDM:startTime="172500" xmpDM:duration="1000"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Part 1" xmpDM:startTime="0" tscDM:image="Lec1_P2_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health, Disease and Illness" xmpDM:startTime="6300" tscDM:image="Lec1_P2_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health, Disease and Illness" xmpDM:startTime="39133" tscDM:image="Lec1_P2_Thumbnails.png" tscDM:imageindex="2" tscDM:imageoffset="0" tscDM:imagerect="150, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health, Disease and Illness" xmpDM:startTime="64933" tscDM:image="Lec1_P2_Thumbnails.png" tscDM:imageindex="3" tscDM:imageoffset="0" tscDM:imagerect="225, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Health and Illness" xmpDM:startTime="141767" tscDM:image="Lec1_P2_Thumbnails.png" tscDM:imageindex="4" tscDM:imageoffset="0" tscDM:imagerect="300, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="How do we think about  Health &amp; Illness?  A ‘cold’" xmpDM:startTime="172500" tscDM:image="Lec1_P2_Thumbnails.png" tscDM:imageindex="5" tscDM:imageoffset="0" tscDM:imagerect="375, 0, 75, 46"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="1AE86508-7B76-4A18-99DD-209BED8C093B" tscIQ:authoredEmail="" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="NONE" tscIQ:clientId="1CDF22D874AC443FBBB68075B4AE31CD" tscIQ:hideReplay="0" tscIQ:quizHash="60b3bff9d58da5bd31d262cc4df20d26">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="255567" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 1"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>How can we define disease? </tscIQ:question><tscIQ:correctAnswer>1</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>An anatomical or physiological abnormality.</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>The personal experience of suffering</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>Physical, psychological and social wellbeing </tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="3"><tscIQ:answer>Behavioural adaptations to the environment</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="Previous"/><rdf:li xmpDM:name="txtNext" xmpDM:value="Next"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="Take Quiz Now"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="Submit Answers"/><rdf:li xmpDM:name="txtReview" xmpDM:value="Replay Last Section"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="View Answers"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="Continue"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Lec1_P2.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="Lec1_P2_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="Lec1_P2_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
