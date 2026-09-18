9aa33ba9d84c9d6683a6548c3343a7461f59f43c
abdullahmassraf <132116762+abdullahmassraf@users.noreply.github.com>
2026-09-17 21:30:07 -0400

    Add files via upload

---
+++ Sheridan/Anthropology of Health ANTH17028GD/Unit01/Lec1_P1/scripts/config_xml.js
@@ -0,0 +1,62 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description tsc:version="2.0.1" dc:date="2015-06-09 10:46:02 AM" dc:source="Camtasia Studio,8.5.2,enu" dc:title="Lec1_P1" tscDM:firstFrame="Lec1_P1_First_Frame.png" tscDM:originId="DFE31CF5-9F8F-4607-86EF-627AFD27B5DB" tscDM:project="Untitled">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="38033"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="720" stDim:w="1152"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Caption" xmpDM:frameRate="f1000" xmpDM:trackName="Captioning" stFnt:fontFamily="Arial" tscDM:fontSize="37" tscDM:bgOpacity="0.500000" tscDM:position="overlay">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:duration="11233" xmpDM:startTime="0" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="en-US">{\\rtf1 Approaches to understanding, health, disease \\par and illness}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tsc:fgColor xmpG:red="255" xmpG:green="255" xmpG:blue="255"/><tsc:bgColor xmpG:red="0" xmpG:green="0" xmpG:blue="0"/></rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="ScreenText" xmpDM:frameRate="f1000" xmpDM:trackName="Screen Text">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Anthropology of Health Module 1 Anthropological Perspectives on Health ARTSTOR_103_41822001543212.jpg" xmpDM:startTime="0" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Pompeii: floor mosaic: skeleton of a cup-bearer." xmpDM:startTime="0" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="2nd C." xmpDM:startTime="0" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="BC." xmpDM:startTime="0" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Museo archeologico nazionale di Napoli." xmpDM:startTime="0" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="ARTstor" xmpDM:startTime="0" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Module 1 Learning Outcomes Define health, disease and illness Understand medical and biological anthropology perspectives on health Evaluate the fundamental" xmpDM:startTime="11233" xmpDM:duration="1000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="principles of the anthropology of health Explore the determinants of health Examine regional variation in the determinants of health" xmpDM:startTime="11233" xmpDM:duration="1000"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Anthropology of Health" xmpDM:startTime="0" tscDM:image="Lec1_P1_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Module 1 Learning Outcomes" xmpDM:startTime="11233" tscDM:image="Lec1_P1_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 46"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Lec1_P1.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="Lec1_P1_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="Lec1_P1_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
